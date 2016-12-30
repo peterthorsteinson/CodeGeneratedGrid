2385b8c55aa33c813fe965b45e12270cca59665c
Peter <peterteach@gmail.com>
2016-12-30 13:12:47 -0800

    Initial code added

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    GameObject target;
+    float viewAngle = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //target = transform;
+        target = GameObject.Find("CodeGeneratedGrid");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(0.0f, 0.0f, 0.0f));
+        viewAngle = 100 * Time.deltaTime;
+        transform.RotateAround(
+            target.transform.position,
+            Vector3.up,
+            viewAngle);
+    }
+}
